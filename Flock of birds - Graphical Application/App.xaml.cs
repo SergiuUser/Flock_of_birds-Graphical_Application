d6394279d73b9920b26396165cd3294087c631c9
Sergiu <soroceansergiu10@gmail.com>
2024-03-15 20:30:18 +0200

    Add project files.

---
+++ Flock of birds - Graphical Application/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Flock_of_birds___Graphical_Application
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
